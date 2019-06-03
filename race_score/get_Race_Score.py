cfa412cf24cde1224996294d54af73d6769eed0e
Sosososj <youamg2@126.com>
2019-06-03 23:48:57 +0800

    modified scrapy race_comment and need to modify the tp_list in userinfo

--- race_score/get_Race_Score.py
+++ race_score/get_Race_Score.py
@@ -7,19 +7,106 @@ from lxml import etree
 import pandas as pd
 import time
 import re
+import math
 
-based_comment_site='http://iranshao.com/races/{}/comments'
-race_id=2740
+based_comment_site = 'http://iranshao.com/races/{}/comments'
+race_id = 2740
 
 
 # print(html)
 
-def test_race_state(race_id):
+def get_race_state(race_id):
     race_comment_site = based_comment_site.format(str(race_id))
     response = requests.get(race_comment_site).text
-    selctor=etree.HTML(response)
-    comment_all_num=selctor.xpath('//div[@id="racecomments"]/div/h2/span/span/text()')
-    comment_site_num=selctor.xpath('//ul[@class="pagination pagination-v1"]/li/a/text()')[-2]
-    return comment_site_num
+    selctor = etree.HTML(response)
+    comment_all_num = selctor.xpath('//div[@id="racecomments"]/div/h2/span/span/text()')
+    comment_site_num = selctor.xpath('//ul[@class="pagination pagination-v1"]/li/a/text()')[-2]
+    return response, comment_site_num
 
-print(test_race_state(583))
+
+def get_comment_list(url_response):
+    selctor = etree.HTML(url_response)
+    user_comment_list = selctor.xpath('//ol[@id="race_comment_tab"]/li')
+    user_comment_len = len(user_comment_list)
+    return user_comment_list
+
+
+def get_comment_userinfo(comments_list,response_text):
+    user_name_list = []
+    user_id_list = []
+
+    for user in comments_list:
+        user_id = user.xpath('h4/a/@href')[0]
+        user_name = user.xpath('h4/a/@title')[0]
+        user_tp = re.findall('<p class="result">(.*?)class="comment-score">',race_response,re.S)
+        for item in user_tp:
+            tp_time=re.findall('<span>(.*?)</span>',item,re.S)
+            print(1)
+
+        user_id_list.append(user_id)
+        user_name_list.append(user_name)
+        # print(user_id)
+        # print(user_name)
+        # print(user_tp)
+    return user_id_list, user_name_list
+
+
+def get_comment_scoreinfo(comments_list):
+    score_s = []
+    score_o = []
+    score_a = []
+    score_m = []
+    for user in comments_list:
+        score_state = user.xpath('div[@class="comment-score"]/span/span[@class="score-item"]')
+        if len(score_state) == 3:
+            score_sight_str = score_state[0].xpath('span/@data-score')[0]
+            score_sight = int(float(score_sight_str))
+            score_organize_str = score_state[1].xpath('span/@data-score')[0]
+            score_organize = int(float(score_organize_str))
+            score_atomsphere_str = score_state[2].xpath('span/@data-score')[0]
+            score_atomsphere = int(float(score_atomsphere_str))
+            score_mean = (score_sight + score_organize + score_atomsphere) / 3
+        elif len(score_state) == 2:
+            score_sight_str = score_state[0].xpath('span/@data-score')[0]
+            score_sight = int(float(score_sight_str))
+            score_organize_str = score_state[1].xpath('span/@data-score')[0]
+            score_organize = int(float(score_organize_str))
+            score_atomsphere = None
+            score_mean = (score_sight + score_organize) / 2
+
+        score_s.append(score_sight)
+        score_o.append(score_organize)
+        score_a.append(score_atomsphere)
+        score_m.append(score_mean)
+
+    return score_s, score_o, score_a, score_m
+
+
+def get_comment_dinfo(comments_list):
+    comments_all = []
+    for user in comments_list:
+        comment_state = user.xpath('div[@class="comment-body"]/p/text()')
+        if comment_state != []:
+            comment = comment_state[0]
+        else:
+            comment = 'null'
+        comments_all.append(comment)
+    return comments_all
+
+
+def get_comment_time(comments_list):
+    commit_time = []
+    for user in comments_list:
+        time_state = user.xpath('p[@class="comment-meta"]/span/text()')
+        if time_state != []:
+            time = time_state[0]
+        else:
+            time = 'null'
+        commit_time.append(time)
+    return commit_time
+
+
+race_response = get_race_state(2740)[0]
+comment_list = get_comment_list(race_response)
+get_comment_userinfo(comment_list,race_response)
+# print(test_race_state(583))
